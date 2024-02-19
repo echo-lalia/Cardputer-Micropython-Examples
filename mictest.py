0efb9e24d75eea9dab15cf3fb4103dff0059b4cc
Ethan <108598670+echo-lalia@users.noreply.github.com>
2024-02-18 16:22:33 -0800

    Update mictest.py
    Has been changed to actually deconstruct the bytes into 1-bit pdm values, and take their average.

--- mictest.py
+++ mictest.py
@@ -8,7 +8,33 @@ import neopixel
 ledPin = Pin(21)
 led = neopixel.NeoPixel(ledPin, 1, bpp=3)
 
+@micropython.viper
+def get_bits(byte:int):
+    return (
+        (byte >> 7) & 1,
+        (byte >> 6) & 1,
+        (byte >> 5) & 1,
+        (byte >> 4) & 1,
+        (byte >> 3) & 1,
+        (byte >> 2) & 1,
+        (byte >> 1) & 1,
+        byte & 1
+        )
 
+def avg_amplitude(buf, samples):
+    reading = 0
+    
+    for i in range(0,samples - 1):
+        bits = get_bits(buf[i])
+        for bit in bits:
+            reading += bit
+    
+    reading /= samples * 8
+    #print(f'{reading},')
+    #time.sleep(1)
+
+    return abs(reading - 0.5)
+    
 
 #init the PDM microphone.
 #I struggled to find real documentation on how this mic should be used, but it seems very picky about its sample rate. 22050 seems to work. 
@@ -34,27 +60,35 @@ audio_in = I2S(
     ibuf=BUFFER_LENGTH_IN_BYTES,
     )
 
+num_multi_samples = 30
 
-
-num_samples = 100
-buf = bytearray(2)
-for i in range(0,1000):
+num_samples = 16
+buf = bytearray(num_samples)
+counter = 0
+start_time = time.ticks_ms()
+while True:
     
-    #take samples and average them
-    value_sum = 0
-    for j in range (num_samples):
-        audio_in.readinto(buf)
-        value_sum += (int(buf[0]) + int(buf[1])) // 2
+    # get amplitude several times, for accuracy
+    multi_sample = []
+    for i in range(0,num_multi_samples):
+        #take samples and average them
+        samples_read = audio_in.readinto(buf)
+        multi_sample.append(avg_amplitude(buf, samples_read))
     
-    val = value_sum // num_samples
+    amplitude = max(multi_sample)
     
-    amplitude = abs(val - 130)
+    #g=math.floor(min(max(amplitude - 0.04, 0) * 1000, 255))
+    #b=math.floor(min((max(amplitude - 0.04, 0) ** 2) * 100000, 255))
     
-    led.fill((amplitude,(amplitude ** 2) // 257,(amplitude ** 2) // 257))
+    led.fill((
+        math.floor(min((max(amplitude - 0.05, 0) ** 3) * 100000, 255)),
+        math.floor(min(max(amplitude - 0.04, 0) * 1000, 255)),
+        math.floor(min((max(amplitude - 0.04, 0) ** 2) * 100000, 255))))
+    
     led.write()
-    
     #time.sleep(0.05)
 
 led.fill((0,0,0))
 led.write()
 audio_in.deinit()
+
